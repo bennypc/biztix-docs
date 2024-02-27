c8919067818634b436905d154b5bb8302ccded51
bennypc <52959626+bennypc@users.noreply.github.com>
2024-02-26 21:52:33 -0500

    eslint settings

--- biztix-docs/next.config.mjs
+++ biztix-docs/next.config.mjs
@@ -4,7 +4,6 @@ import withSearch from './src/markdoc/search.mjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
-  reactStrictMode: true,
 }
 
 export default withSearch(
